64664bece7194e246b66be9f16324a8ef8b336f6
Upendra Singh <upendra.singh@mindtickle.com>
2023-09-22 00:47:02 +0530

    Changed in main file code

--- api/main.go
+++ api/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"URL-Shortner/routes"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/joho/godotenv"
-	"github.com/nenite18/URL-Shortner/api/routes"
 	"log"
 	"os"
 )
@@ -24,6 +24,7 @@ func main() {
 	app := fiber.New()
 	app.Use(logger.New())
 	setupRoutes(app)
+	fmt.Println("Starting")
 
 	log.Fatal(app.Listen(os.Getenv("APP_PORT")))
 }
